d3ab694bd59b552d15c987f7e257e297acd49bdd
BrujoFurioso22 <diegobarbecho133@gmail.com>
2024-01-24 17:32:23 -0500

    datos

--- programa.py
+++ programa.py
@@ -2,4 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO('https://github.com/BrujoFurioso22/Yolo/blob/main/best.pt')
 
-results = model('')
+results = model('https://github.com/BrujoFurioso22/Yolo/blob/main/imagenes/c1.png')
+
+print(results)
