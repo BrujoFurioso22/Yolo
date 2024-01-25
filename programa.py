fccff5519b76cda0eef2ffeab386df9bd70a662f
BrujoFurioso22 <diegobarbecho133@gmail.com>
2024-01-25 07:17:39 -0500

    datos

--- programa.py
+++ programa.py
@@ -1,30 +1,23 @@
-from pyspark.sql import SparkSession
-from ultralytics import YOLO
-
-# Inicializar la sesión de Spark
-spark = SparkSession.builder.appName("YOLOExample").getOrCreate()
-
-# Cargar el modelo YOLO en cada nodo del clúster
-model = YOLO('hdfs://master:9000/user/hadoop/Yolo/best.pt')
-
-# Crear una lista de rutas de imágenes que deseas procesar
-imagen_paths = ['hdfs://master:9000/user/hadoop/Yolo/imagenes/c1.png']
-
-# Distribuir la lista de rutas como un RDD
-imagen_paths_rdd = spark.sparkContext.parallelize(imagen_paths)
-
-# Aplicar el modelo YOLO a cada imagen en paralelo
-results_rdd = imagen_paths_rdd.map(lambda path: model(path))
-
-# Recopilar los resultados en el nodo maestro (puede ser costoso si hay muchos resultados)
-results = results_rdd.collect()
-
-# Imprimir los resultados
-for result in results:
-    print(result)
-
+from pyspark.sql import SparkSession 
+from pyspark import SparkFiles 
+from ultralytics import YOLO 
+# Inicializar la sesión de Spark 
+spark = SparkSession.builder.appName("YOLOExample").getOrCreate() 
+# Ruta al modelo YOLO en HDFS 
+ruta_modelo_hdfs = "hdfs://master:9000/user/hadoop/Yolo/best.pt" 
+ruta_local="/home/hadoop/proyecto/Yolo/best.pt" 
+#Copiar el modelo YOLO al sistema de archivos local del nodo maestro 
+spark.sparkContext.addFile(ruta_modelo_hdfs) 
+ruta_modelo_local = 'file://'+SparkFiles.get('best.pt') 
+# Cargar el modelo YOLO localmente 
+modelo = YOLO (ruta_local) 
+# Crear una lista de rutas de imágenes que deseas procesar 
+ruta_imagen_hdfs = "hdfs://master:9000/user/hadoop/Yolo/imagenes/c1.png" 
+# Cargar la imagen directamente desde HDFS 
+imagen = spark.read.format("image").option("drop Invalid", True).load(ruta_imagen_hdfs) 
+#Realizar inferencia en la imagen 
+resultado = modelo (imagen) 
 # Guardar los resultados en un archivo de texto en HDFS
-results_rdd.saveAsTextFile('hdfs://master:9000/user/hadoop/Yolo/resultados/')
-
-# Detener la sesión de Spark
+resultado.saveAsTextFile("hdfs://master:9000/user/hadoop/Yolo/imagenes/test1.txt") 
+#Detener la sesión de Spark 
 spark.stop()
